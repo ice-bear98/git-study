5b8001e87b9d1bcfcf8e25ff4cd7edeb43a53a9b
seungchanwoo <tmdcksqkd123@naver.com>
2023-05-24 23:00:30 +0900

    수정: 시계 기능수정
    0~9초에서 1자리수만 표시되는것을 수정함

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ const clock = document.querySelector(".clock");
 
 function getClock() {
   const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const Seconds = date.getSeconds();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const Seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${Seconds}`;
 }
 
