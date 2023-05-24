a659294b6c45057ed98f51f535c22abe03b7eebf
seungchanwoo <tmdcksqkd123@naver.com>
2023-05-24 22:50:09 +0900

    feat: 시계 레이아웃 및 기능추가
    1.시계 레이아웃 작성
    2.시계 스타일 작성
    3.시계 기능추가

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 alert("로그인 페이지입니다.");
+
+const clock = document.querySelector(".clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const Seconds = date.getSeconds();
+  clock.innerText = `${hours}:${minutes}:${Seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
